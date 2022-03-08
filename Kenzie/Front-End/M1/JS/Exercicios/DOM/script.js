65874776b2a7e0c61d0a95e0e88b64b540789324
Antoniel Mariano <antonielcamposm@gmail.com>
2022-03-08 12:21:20 -0300

    update: exercicios DOM

---
+++ Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
@@ -0,0 +1,8 @@
+let tag = document.createElement('p')
+console.log(tag)
+
+tag.innerText = "Olá, eu sou um paragrafo em DOM"
+
+document.querySelector('body').appendChild(tag)
+/////////////////////
+
