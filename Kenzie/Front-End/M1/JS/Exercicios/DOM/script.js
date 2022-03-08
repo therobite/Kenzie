7239788dbbb81e9f0f2687168f32c317f670c9bc
Antoniel Mariano <antonielcamposm@gmail.com>
2022-03-08 12:26:06 -0300

    Update: Exercicios DOM

--- Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
+++ Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
@@ -1,8 +1,12 @@
 let tag = document.createElement('p')
-console.log(tag)
-
 tag.innerText = "Olá, eu sou um paragrafo em DOM"
 
 document.querySelector('body').appendChild(tag)
 /////////////////////
 
+const tagInput = document.createElement('input')
+const form = document.querySelector('form')
+
+tagInput.placeholder = 'Digite seu texto aqui'
+form.appendChild(tagInput)
+////////////////////
