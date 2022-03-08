800c455b6a1575fad1832cad95252f659ccbcfc3
Antoniel Mariano <antonielcamposm@gmail.com>
2022-03-08 14:06:40 -0300

    Update: Exercicios DOM

--- Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
+++ Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
@@ -63,7 +63,59 @@ tagP.appendChild(novo)
  */
 //////////////////////
 
+/* const shoppingList = document.getElementById("list")
+const input = document.getElementById("input")
+const addButton = document.getElementById("add")
+const resetButton = document.getElementById("reset")
+
+let list = []
 
 
+function reloadList(){
+    shoppingList.innerHTML = ""
+    for(let i = 0; i < list.length; i++){
+        currentElement = list[i]
+        currentElement.id = i
+        shoppingList.appendChild(currentElement)
+    }
+}
 
+function createRemoveButton(){
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove"
+    removeButton.addEventListener("click", (e) => {
+        list = list.filter((_, index) => index !== Number(e.path[1].id))
+        
+        reloadList();
+    })
+    return removeButton
 
+}
+
+function onClickFunction (){
+    let newItem =  document.createElement("li")
+    let paragraph = document.createElement("p")
+    let removeButton = createRemoveButton()
+
+    if(input.value){
+        paragraph.innerText = input.value;
+
+        newItem.appendChild(paragraph)
+        newItem.appendChild(removeButton)
+
+        input.value = ""
+        list.push(newItem)
+
+        reloadList()
+
+    }
+}
+addButton.addEventListener("click", onClickFunction);
+
+function onClickResetFunction(){
+    shoppingList.innerHTML=""
+    list = []
+}
+
+resetButton.addEventListener("click", onClickResetFunction)
+ */
