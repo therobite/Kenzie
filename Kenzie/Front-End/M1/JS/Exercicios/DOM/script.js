b9c05f3229d500d4085d1ac4fb7782d1fb3d020f
Antoniel Mariano <antonielcamposm@gmail.com>
2022-03-08 12:43:02 -0300

    Update: Exercicios DOM

--- Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
+++ Kenzie/Front-End/M1/JS/Exercicios/DOM/script.js
@@ -1,12 +1,29 @@
-let tag = document.createElement('p')
+/* let tag = document.createElement('p')
 tag.innerText = "Olá, eu sou um paragrafo em DOM"
 
-document.querySelector('body').appendChild(tag)
+document.querySelector('body').appendChild(tag) */
 /////////////////////
 
-const tagInput = document.createElement('input')
+/* const tagInput = document.createElement('input')
 const form = document.querySelector('form')
 
 tagInput.placeholder = 'Digite seu texto aqui'
-form.appendChild(tagInput)
+form.appendChild(tagInput) */
 ////////////////////
+/* 
+let article = document.createElement('article')
+let articleH1 = document.createElement('h1')
+let articleP = document.createElement('p')
+let articleA= document.createElement('a')
+document.querySelector('body').appendChild(article)
+
+articleH1.innerText = 'DOM é muito bom'
+articleP.innerText = 'DOM é complexo'
+articleA.innerText = 'Para saber mais clique aqui'
+
+document.querySelector('article').appendChild(articleH1)
+document.querySelector('article').appendChild(articleP)
+document.querySelector('article').appendChild(articleA) */
+////////////////////
+
+
